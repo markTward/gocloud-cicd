8e15dbac975aec63191596e90e22af5f39bf7568
markTward <mark.t.ward@gmail.com>
2017-04-19 18:06:37 -0700

    conditional logic to handle multiple registries based on branch

--- travis/docker_push.sh
+++ travis/docker_push.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
-
+# TODO: handle pull request
 echo "docker_push.sh script start"
-
 docker version
 
-echo "docker repo: $DOCKER_REPO"
-echo "docker commit tag: $DOCKER_COMMIT_TAG"
-echo "docker build/push for TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE"
+BRANCH_REGEX="^(master$|release//*|^pr/.*)"
+
+if [[ $TRAVIS_BRANCH =~ $BRANCH_REGEX ]];
+  then
+    DOCKER_REPO=gcr.io/GCLOUD_PROJECT_ID/$GOCLOUD_PROJECT_NAME;
+  else
+    DOCKER_REPO=$(echo $TRAVIS_REPO_SLUG | tr '[:upper:]' '[:lower:]');
+fi
+
+echo "DOCKER_REPO: $DOCKER_REPO"
+echo "DOCKER_COMMIT_TAG: $DOCKER_COMMIT_TAG"
+echo "TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE"
+echo "BRANCH_REGEX: $BRANCH_REGEX"
+echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+echo "TRAVIS_EVENT_TYPE: $TRAVIS_EVENT_TYPE"
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
 
 if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]; then
-  docker tag $DOCKER_REPO:$DOCKER_COMMIT_TAG $DOCKER_REPO:PR-$TRAVIS_PULL_REQUEST;
+  docker tag $GOCLOUD_PROJECT_NAME:$DOCKER_COMMIT_TAG $DOCKER_REPO:PR-$TRAVIS_PULL_REQUEST;
 fi
 
 if [ "$TRAVIS_EVENT_TYPE" == "push" ]; then
-  docker tag $DOCKER_REPO:$DOCKER_COMMIT_TAG $DOCKER_REPO:$TRAVIS_BRANCH;
+  docker tag $GOCLOUD_PROJECT_NAME:$DOCKER_COMMIT_TAG $DOCKER_REPO:$TRAVIS_BRANCH;
   if [ "$TRAVIS_BRANCH" == "master" ]; then
-    docker tag $DOCKER_REPO:$DOCKER_COMMIT_TAG $DOCKER_REPO:latest;
+    docker tag $GOCLOUD_PROJECT_NAME:$DOCKER_COMMIT_TAG $DOCKER_REPO:latest;
   fi
 fi
 
