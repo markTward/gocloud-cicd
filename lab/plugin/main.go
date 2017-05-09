75e8a8d89f21fad2b8321fc4813ea095a0545b04
markTward <mark.t.ward@gmail.com>
2017-05-09 14:21:05 -0700

    pluggable registry with copy from generic to specifc for properties

--- lab/plugin/main.go
+++ lab/plugin/main.go
@@ -2,51 +2,86 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"reflect"
+	"strings"
 
 	yaml "gopkg.in/yaml.v2"
 )
-
-type GCRRegistry struct {
-	Name        string
-	Description string
-	Host        string
-	Project     string
-	Repo        string
-	Url         string
-	KeyFile     string
-}
-
-func (r *GCRRegistry) ListProperties() string {
-	return fmt.Sprintf("GCR Properties: %#v", r)
-}
-
-type DockerRegistry struct {
-	Name        string
-	Description string
-	Host        string
-	Account     string
-	Repo        string
-	Url         string
-}
-
-func (r *DockerRegistry) ListProperties() string {
-	return fmt.Sprintf("Docker Properties: %#v", r.ListProperties())
-}
-
-type Registrator interface {
-	ListProperties() map[string]string
-}
 
 type Registry struct {
 	Name       string
 	Properties map[string]string
 }
 
-func (r *Registry) ListProperties() map[string]string {
-	fmt.Printf("ListProps: %#v\n", r)
-	return r.Properties
+type Registrator interface {
+	ListProperties()
+	Copy(Registry)
 }
+
+type GCRRegistry struct {
+	Name       string
+	Properties struct {
+		Description string
+		Host        string
+		Project     string
+		Repo        string
+		Url         string
+		KeyFile     string
+	}
+}
+
+func (r *GCRRegistry) Copy(src Registry) {
+
+	r.Name = src.Name
+	s := reflect.ValueOf(&r.Properties).Elem()
+	typeOfT := s.Type()
+
+	for i := 0; i < s.NumField(); i++ {
+		n := typeOfT.Field(i).Name
+		v := src.Properties[strings.ToLower(n)]
+		reflect.ValueOf(&r.Properties).Elem().FieldByName(n).SetString(v)
+	}
+
+}
+
+func (r *GCRRegistry) ListProperties() {
+	log.Println("GCR ListProperties", r.Properties)
+}
+
+type DockerRegistry struct {
+	Name       string
+	Properties struct {
+		Description string
+		Host        string
+		Account     string
+		Repo        string
+		Url         string
+	}
+}
+
+func (r *DockerRegistry) Copy(src Registry) {
+
+	r.Name = src.Name
+	s := reflect.ValueOf(&r.Properties).Elem()
+	typeOfT := s.Type()
+
+	for i := 0; i < s.NumField(); i++ {
+		n := typeOfT.Field(i).Name
+		v := src.Properties[strings.ToLower(n)]
+		reflect.ValueOf(&r.Properties).Elem().FieldByName(n).SetString(v)
+	}
+
+}
+
+func (r *DockerRegistry) ListProperties() {
+	log.Println("GCR ListProperties", r.Properties)
+}
+
+// func (r *Registry) ListProperties() map[string]string {
+// 	fmt.Printf("ListProps: %#v\n", r)
+// 	return r.Properties
+// }
 
 type Config struct {
 	Registry
@@ -71,17 +106,17 @@ func makeInstance(name string) interface{} {
 }
 
 func main() {
-	gdata := `
-registry:
-  name: gcr
-  properties:
-    description: Google Container Registry
-    host: gcr.io
-    project: k8sdemo-159622
-    repo: gocloud
-    url: gcr.io/k8sdemo-159622/gocloud
-    keyfile: ./client-secret.json
-`
+	// 	gdata := `
+	// registry:
+	//   name: gcr
+	//   properties:
+	//     description: Google Container Registry
+	//     host: gcr.io
+	//     project: k8sdemo-159622
+	//     repo: gocloud
+	//     url: gcr.io/k8sdemo-159622/gocloud
+	//     keyfile: ./client-secret.json
+	// `
 	ddata := `
 registry:
   name: docker
@@ -92,35 +127,32 @@ registry:
     repo: gocloud
     url: docker.io/marktward/gocloud
 `
-	fmt.Println("GRC Data:", gdata)
-	fmt.Println("Docker Data:", ddata)
+	fmt.Printf("Type Registry: %#v\n\n", typeRegistry)
 
-	data := gdata
+	data := ddata
 	cfg := Config{}
 	if err := LoadConfig(data, &cfg); err != nil {
 		fmt.Println("error:", err)
 	}
 	fmt.Printf("Config: %#v\n\n", cfg)
 
-	fmt.Printf("Type Registry: %#v\n\n", typeRegistry)
 	var activeRegistry interface{}
 	switch cfg.Registry.Name {
 	case "gcr":
-		activeRegistry = makeInstance("gcr")
-		activeRegistry = &cfg.Registry
+		x := makeInstance("gcr").(GCRRegistry)
+		activeRegistry = &x
 	case "docker":
-		activeRegistry = makeInstance("docker")
-		activeRegistry = &cfg.Registry
+		x := makeInstance("docker").(DockerRegistry)
+		activeRegistry = &x
 	default:
 		fmt.Println("unknown registry")
 	}
+	fmt.Printf("Active Registry: %#v %T\n\n", activeRegistry, activeRegistry)
 
-	fmt.Printf("Active Registry: %#v %T\n", activeRegistry, activeRegistry)
 	ar := activeRegistry.(Registrator)
-	// &activeRegistry.ListProperties()
+	fmt.Printf("AR asserted: %#v %T\n\n", ar, ar)
 
-	// ar := r.(Registrator.ListProperties())
-	fmt.Printf("AR Registrator: %#v\n\n", ar)
-
-	fmt.Printf("AR Properties: %#v\n", ar.ListProperties())
+	ar.Copy(cfg.Registry)
+	fmt.Printf("Active Registry after COPY: %#v\n\n", activeRegistry)
+	ar.ListProperties()
 }
