bfb07aa54b62d0ba45d8031f4ea8fde0ebc48831
markTward <mark.t.ward@gmail.com>
2017-05-13 18:17:31 -0700

    working helm values render example

--- lab/templates/main.go
+++ lab/templates/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"log"
 	"os"
 	"text/template"
@@ -9,33 +11,56 @@ import (
 func main() {
 	// Define a template.
 	const yaml = `
-service:
-  gocloudAPI:
-    image:
-      repository: {{.Repo}}
-      tag: {{.Tag}}
-  gocloudGrpc:
-    image:
-      repository: {{.Repo}}
-      tag: {{.Tag}}
+  service:
+    gocloudAPI:
+      image:
+        repository: {{.Repo}}
+        tag: {{.Tag}}
+    gocloudGrpc:
+      image:
+        repository: {{.Repo}}
+        tag: {{.Tag}}
 `
-
 	// Prepare some data to insert into the template.
-	type Value struct {
-		Repo string
-		Tag   string
+	type Values struct {
+		Repo, Tag string
 	}
-
-  var repo = "gcr.io/k8s-158622/gocloud"
-  var tag = "198273adf"
-  var values = []Value{{repo, tag}}
+	var values = []Values{
+		{"gcr.io/k8s-158622", "ada89f7da8"},
+	}
 
 	// Create a new template and parse the letter into it.
 	t := template.Must(template.New("yaml").Parse(yaml))
 
-	// Execute the template for each recipient.
-	for _, v := range values {
-		err := t.Execute(os.Stdout, v)
+	// write to Stdout
+	fmt.Println("stdout")
+	for _, r := range values {
+		err := t.Execute(os.Stdout, r)
+		if err != nil {
+			log.Println("executing template:", err)
+		}
+	}
+
+	// write to var
+	var rt bytes.Buffer
+	for _, r := range values {
+		err := t.Execute(&rt, r)
+		if err != nil {
+			log.Println("executing template:", err)
+		}
+	}
+	fmt.Println("bytes.Buffer:", rt.String())
+
+	// write to file
+	fmt.Println("write to file")
+	f, err := os.Create("./runtime_values.yaml")
+	if err != nil {
+		log.Println("create file: ", err)
+		return
+	}
+	defer f.Close()
+	for _, r := range values {
+		err := t.Execute(f, r)
 		if err != nil {
 			log.Println("executing template:", err)
 		}
